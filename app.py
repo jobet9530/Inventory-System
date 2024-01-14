02cb269a59f50c437329635e5ad1129250610c89
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-01-14 12:25:15 +0000

    feat: Add Flask import to app.py and remove unused imports from app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for, redirect, jsonify
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from database import database
 from flask import Flask
