dc7515b5e635c75cffb682eefec58e1b9553989e
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-01-14 06:15:02 +0000

    Update 2 files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from database import db, Product, Customer
+from database import database
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=3300)
