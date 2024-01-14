67e58843af691dac89ddefc28ccbc40541017d03
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-01-14 05:50:46 +0000

    feat: Fix importing Customer and add route for handling products

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from database import db, Product
+from database import db, Product, Customer
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/products/', methods=['GET', 'POST'])
+def products():
 
 
 if __name__ == '__main__':
