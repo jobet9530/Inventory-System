3e0bfda5f8ccc10f1932c339573912a5cfc7b719
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-01-14 05:51:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/products/', methods=['GET', 'POST'])
-def products():
-
-
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
